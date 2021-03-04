86160eda5130f0e07676149d2278ab0f3877ad48
ajcwebdev <anthony@stepzen.com>
2021-03-04 12:14:19 -0800

    Initial commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <h1>About</h1>
+      <p>This page tells you about stuff!</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
